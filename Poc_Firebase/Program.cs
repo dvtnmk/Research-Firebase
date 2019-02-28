5dcb785692384a6916583d7959483ae608124463
DvtNmk <devotion.nk@gmail.com>
2019-02-28 14:30:50 +0700

    [Add]Connection to Firebase

--- Poc_Firebase/Program.cs
+++ Poc_Firebase/Program.cs
@@ -3,13 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Cloud.Firestore;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
 
 namespace Poc_Firebase
 {
-    class Program
+    
+    public class Program
     {
-        static void Main(string[] args)
+        public static IFirebaseConfig config = new FirebaseConfig
         {
+            AuthSecret = "dJVmRiqZnCsBFyJd9EQ64xOv9cINBCHrYhl6Jv9v",
+            BasePath = "https://poc-firebase-7735b.firebaseio.com/"
+        };
+
+        public static IFirebaseClient client;
+
+        public static void Main(string[] args)
+        {
+            client = new FireSharp.FirebaseClient(config);
+
+            if(client != null)
+            {
+                Console.WriteLine("Connection is established");
+            }
+            else
+            {
+                Console.WriteLine("Fail to Connection");
+            }
+            Console.ReadKey();
+
         }
     }
 }
